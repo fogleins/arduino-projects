356bd6d928d02a7f8287d0fb3f47f0a907f0baea
44982432 <fogleins@hotmail.hu>
2020-12-20 01:08:45 +0100

    Add logging

--- 010_lcd_temp_alcsensor_serial/serial_reader.py
+++ 010_lcd_temp_alcsensor_serial/serial_reader.py
@@ -6,14 +6,20 @@ import plotly.express as px
 import asyncio
 import datetime
 import csv
+import logging
 
 ARDUINO_PORT = "/dev/ttyUSB0"
+# the number of minutes after the plot will be updated
+PLOT_INTERVAL = 30
 
 async def main():
     ser = serial.Serial(ARDUINO_PORT)
     ser.flushInput()
 
-    count = 0
+    logging.basicConfig(filename="./plots.log", filemode="a", encoding="utf-8",
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+    count = 1
     while True:
         now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
@@ -29,25 +35,32 @@ async def main():
             writer = csv.writer(f)
             writer.writerow([now, parsed_bytes])
 
-        # we update the graph every ten minutes
-        if count == 9:
+        # updating the plot
+        if count == PLOT_INTERVAL:
             await update_temperature_plot(now)
             await update_co_plot(now)
-            count = -1
-        count += 1
+            count = 1
+        else:
+            count += 1
 
 async def update_temperature_plot(updated_at):
+    started_at = datetime.datetime.now()
     data = pd.read_csv("temperature_data.csv", names=["Time", "Temperature"])
     x_values = data["Time"]
     y_values = data["Temperature"]
     fig = px.scatter(x=x_values, y=y_values, title=f"Hőmérséklet az idő függvényében (frissítve: {updated_at})")
     fig.write_html("temperature_plot.html")
+    time_taken = datetime.datetime.now() - started_at
+    logging.info(f"Finished building temperature plot in {time_taken}")
 
 async def update_co_plot(updated_at):
+    started_at = datetime.datetime.now()
     data = pd.read_csv("co_data.csv", names=["Time", "Concentration"])
     x_values = data["Time"]
     y_values = data["Concentration"]
     fig = px.scatter(x=x_values, y=y_values, title=f"CO koncentráció az idő függvényében (frissítve: {updated_at})")
     fig.write_html("co_plot.html")
+    time_taken = datetime.datetime.now() - started_at
+    logging.info(f"Finished building CO plot in {time_taken}")
 
 asyncio.run(main())
