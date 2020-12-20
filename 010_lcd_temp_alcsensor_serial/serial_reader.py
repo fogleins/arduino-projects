567545601b31b600546deb722e4846e233ecd03a
44982432 <fogleins@hotmail.hu>
2020-12-21 00:06:18 +0100

    Change file handling
    -Values are written to csv files just before a plot is built instead of every time a value is read
    -Minor changes to meet Python code formatting guidelines

--- 010_lcd_temp_alcsensor_serial/serial_reader.py
+++ 010_lcd_temp_alcsensor_serial/serial_reader.py
@@ -12,6 +12,10 @@ ARDUINO_PORT = "/dev/ttyUSB0"
 # the number of minutes after the plot will be updated
 PLOT_INTERVAL = 30
 
+TEMPERATURE_DATA = dict()
+CO_DATA = dict()
+
+
 async def main():
     ser = serial.Serial(ARDUINO_PORT)
     ser.flushInput()
@@ -23,25 +27,36 @@ async def main():
     while True:
         now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-        with open("temperature_data.csv", "a", newline="") as f:
-            input_bytes = ser.readline()
-            parsed_bytes = float(input_bytes[0:len(input_bytes)-2].decode("utf-8"))
-            writer = csv.writer(f)
-            writer.writerow([now, parsed_bytes])
-
-        with open("co_data.csv", "a", newline="") as f:
-            input_bytes = ser.readline()
-            parsed_bytes = float(input_bytes[0:len(input_bytes)-2].decode("utf-8"))
-            writer = csv.writer(f)
-            writer.writerow([now, parsed_bytes])
+        # reading the temperature value
+        input_bytes = ser.readline()
+        TEMPERATURE_DATA[now] = float(input_bytes[0:len(input_bytes)-2].decode("utf-8"))
+        # reading the CO value
+        input_bytes = ser.readline()
+        CO_DATA[now] = float(input_bytes[0:len(input_bytes)-2].decode("utf-8"))
 
         # updating the plot
         if count == PLOT_INTERVAL:
+            write_data_to_csv()
             await update_temperature_plot(now)
             await update_co_plot(now)
             count = 1
         else:
             count += 1
+
+
+def write_data_to_csv():
+    with open("temperature_data.csv", "a", newline="") as f:
+        writer = csv.writer(f)
+        for date, value in TEMPERATURE_DATA.items():
+            writer.writerow([date, value])
+    TEMPERATURE_DATA.clear()
+    
+    with open("co_data.csv", "a", newline="") as f:
+        writer = csv.writer(f)
+        for date, value in CO_DATA.items():
+            writer.writerow([date, value])
+    CO_DATA.clear()
+        
 
 async def update_temperature_plot(updated_at):
     started_at = datetime.datetime.now()
@@ -52,6 +67,7 @@ async def update_temperature_plot(updated_at):
     fig.write_html("temperature_plot.html")
     time_taken = datetime.datetime.now() - started_at
     logging.info(f"Finished building temperature plot in {time_taken}")
+
 
 async def update_co_plot(updated_at):
     started_at = datetime.datetime.now()
